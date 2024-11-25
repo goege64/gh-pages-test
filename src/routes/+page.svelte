7d533a225600635fe1416a8c2f17bf7d3a871195
goege64 <183505282+goege64@users.noreply.github.com>
2024-11-25 09:57:28 -0300

    app base

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let count = $state(0);
+
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<h1>Hello</h1>
+<p>count: {count}</p>
+<button onclick={increment}>Increment</button>
+<a href="/subpage">Hm....</a>
---
+++ src/routes/subpage/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Oh hi</h1>
+<a href="/">Go Back.</a>
